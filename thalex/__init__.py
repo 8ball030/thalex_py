346edaa311f7e6bc255b37788085a6b1cc7a9413
8ball030 <8baller@station.codes>
2025-09-19 16:16:01 +0100

    feat:added-ci-cd-workflows

--- thalex/__init__.py
+++ thalex/__init__.py
@@ -1,1 +1,23 @@
-from thalex.thalex import *
+from .thalex import (
+    Direction,
+    Order,
+    Position,
+    Thalex,
+    ThalexException,
+    ThalexRequestException,
+    ThalexResponseException,
+    Trade,
+    Wallet,
+)
+
+__all__ = [
+    "Thalex",
+    "Direction",
+    "Order",
+    "Position",
+    "Trade",
+    "Wallet",
+    "ThalexException",
+    "ThalexRequestException",
+    "ThalexResponseException",
+]
